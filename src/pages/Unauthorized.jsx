71e09fb26e9bbf45c7d7fc819eba2adccdb192b1
cremeni25 <andersonnavarro.si@gmail.com>
2026-02-27 23:31:09 -0300

    Create Unauthorized.jsx

---
+++ src/pages/Unauthorized.jsx
@@ -0,0 +1,8 @@
+export default function Unauthorized() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h2>Acesso não autorizado</h2>
+      <p>Você não possui permissão para acessar esta área.</p>
+    </div>
+  );
+}

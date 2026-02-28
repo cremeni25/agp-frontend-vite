829739ae6a1b4b7c69bdf001e3ffcc224479e037
cremeni25 <andersonnavarro.si@gmail.com>
2026-02-28 12:54:24 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+// 🔥 IMPORTAÇÃO GLOBAL DE CSS
+import "./styles/home.css";
+import "./styles/dashboard-atleta.css";
+import "./styles/dashboard-comissao.css";
+import "./styles/dashboard-clube.css";
+import "./styles/dashboard-master.css";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />

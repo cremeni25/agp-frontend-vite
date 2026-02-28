dc96bc343cc6ac7b8bb7c12e4a36741464ff8779
cremeni25 <andersonnavarro.si@gmail.com>
2026-02-28 12:47:19 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,94 +1,28 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+// src/App.jsx
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-// Pages
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import ProfileSetup from "./pages/ProfileSetup";
-import Unauthorized from "./pages/Unauthorized";
 
 import DashboardAtleta from "./pages/DashboardAtleta";
 import DashboardComissao from "./pages/DashboardComissao";
 import DashboardClube from "./pages/DashboardClube";
 import DashboardMaster from "./pages/DashboardMaster";
 
-// Guards
-import AuthGuard from "./guards/AuthGuard";
-import ProfileGuard from "./guards/ProfileGuard";
-import CommercialStateGuard from "./guards/CommercialStateGuard";
-
-function App() {
+export default function App() {
   return (
-    <Routes>
-      {/* Rotas públicas */}
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/unauthorized" element={<Unauthorized />} />
+    <BrowserRouter>
+      <Routes>
+        {/* Porta de entrada */}
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
 
-      {/* Setup de perfil */}
-      <Route
-        path="/profile-setup"
-        element={
-          <AuthGuard>
-            <ProfileSetup />
-          </AuthGuard>
-        }
-      />
-
-      {/* Dashboards protegidos */}
-      <Route
-        path="/dashboard/atleta"
-        element={
-          <AuthGuard>
-            <ProfileGuard>
-              <CommercialStateGuard>
-                <DashboardAtleta />
-              </CommercialStateGuard>
-            </ProfileGuard>
-          </AuthGuard>
-        }
-      />
-
-      <Route
-        path="/dashboard/comissao"
-        element={
-          <AuthGuard>
-            <ProfileGuard>
-              <CommercialStateGuard>
-                <DashboardComissao />
-              </CommercialStateGuard>
-            </ProfileGuard>
-          </AuthGuard>
-        }
-      />
-
-      <Route
-        path="/dashboard/clube"
-        element={
-          <AuthGuard>
-            <ProfileGuard>
-              <CommercialStateGuard>
-                <DashboardClube />
-              </CommercialStateGuard>
-            </ProfileGuard>
-          </AuthGuard>
-        }
-      />
-
-      <Route
-        path="/dashboard/master"
-        element={
-          <AuthGuard>
-            <ProfileGuard>
-              <DashboardMaster />
-            </ProfileGuard>
-          </AuthGuard>
-        }
-      />
-
-      {/* Fallback */}
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
+        {/* Dashboards por perfil */}
+        <Route path="/dashboard-atleta" element={<DashboardAtleta />} />
+        <Route path="/dashboard-comissao" element={<DashboardComissao />} />
+        <Route path="/dashboard-clube" element={<DashboardClube />} />
+        <Route path="/dashboard-master" element={<DashboardMaster />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;

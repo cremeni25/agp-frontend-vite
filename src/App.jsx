935b9fe29cdf7ec46263806d65f4694634c43395
cremeni25 <andersonnavarro.si@gmail.com>
2026-08-05 12:38:53 -0300

    Integra rota da equipe técnica

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ import DashboardMaster from "./pages/DashboardMaster";
 import MasterAdministration from "./pages/MasterAdministration";
 import MasterInstitutions from "./pages/MasterInstitutions";
 import MasterProjects from "./pages/MasterProjects";
+import MasterTechnicalTeam from "./pages/MasterTechnicalTeam";
 import MasterUsers from "./pages/MasterUsers";
 import MasterProfiles from "./pages/MasterProfiles";
 import MasterParticipants from "./pages/MasterParticipants";
@@ -52,6 +53,7 @@ function App() {
           <Route path="/dashboard-master/administracao" element={<ProtectedRoute tipoPermitido="master"><MasterAdministration /></ProtectedRoute>} />
           <Route path="/dashboard-master/administracao/instituicoes" element={<ProtectedRoute tipoPermitido="master"><MasterInstitutions /></ProtectedRoute>} />
           <Route path="/dashboard-master/administracao/projetos" element={<ProtectedRoute tipoPermitido="master"><MasterProjects /></ProtectedRoute>} />
+          <Route path="/dashboard-master/administracao/equipe-tecnica" element={<ProtectedRoute tipoPermitido="master"><MasterTechnicalTeam /></ProtectedRoute>} />
           <Route path="/master/usuarios" element={<ProtectedRoute tipoPermitido="master"><MasterUsers /></ProtectedRoute>} />
           <Route path="/master/perfis" element={<ProtectedRoute tipoPermitido="master"><MasterProfiles /></ProtectedRoute>} />
           <Route path="/master/participantes" element={<ProtectedRoute tipoPermitido="master"><MasterParticipants /></ProtectedRoute>} />

3dae764bf9632ddd465855ca5f34353f9ccda4b3
cremeni25 <andersonnavarro.si@gmail.com>
2026-09-17 23:39:25 -0300

    AGP UI: use canonical N1 coach invitation flow

--- src/App.jsx
+++ src/App.jsx
@@ -37,6 +37,7 @@ import MasterAnalyticPipeline from "./pages/MasterAnalyticPipeline";
 import MasterProfessionalValidation from "./pages/MasterProfessionalValidation";
 import MasterHomologation from "./pages/MasterHomologationN1";
 import MasterTechnicalAccess from "./pages/MasterTechnicalAccess";
+import MasterInviteN1Coach from "./pages/MasterInviteN1Coach";
 import HomologationEnvironment from "./pages/HomologationEnvironment";
 import CommissionProfessionalValidation from "./pages/CommissionProfessionalValidation";
 import Unauthorized from "./pages/Unauthorized";
@@ -94,7 +95,7 @@ function App() {
           <Route path="/master/pipeline-analitico" element={<ProtectedRoute tipoPermitido="master"><MasterAnalyticPipeline /></ProtectedRoute>} />
           <Route path="/master/validacao-profissional" element={<ProtectedRoute tipoPermitido="master"><MasterProfessionalValidation /></ProtectedRoute>} />
           <Route path="/master/homologacao" element={<ProtectedRoute tipoPermitido="master"><MasterHomologation /></ProtectedRoute>} />
-          <Route path="/master/homologacao/acesso-tecnico" element={<ProtectedRoute tipoPermitido="master"><MasterTechnicalAccess /></ProtectedRoute>} />
+          <Route path="/master/homologacao/acesso-tecnico" element={<ProtectedRoute tipoPermitido="master"><MasterInviteN1Coach /></ProtectedRoute>} />
           <Route path="/master/homologacao/:slug" element={<ProtectedRoute tipoPermitido="master"><HomologationEnvironment /></ProtectedRoute>} />
 
           <Route path="/unauthorized" element={<Unauthorized />} />
